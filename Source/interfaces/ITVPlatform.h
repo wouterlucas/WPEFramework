f68d56ed330670727d5219a13710ca9e0ae9266c
modeveci <o.deveci@metrological.com>
2018-08-23 13:44:03 +0200

    [TVPLATFORM]: Initial commit of new ITVPlatform interface

--- Source/interfaces/ITVPlatform.h
+++ Source/interfaces/ITVPlatform.h
@@ -1,130 +1,154 @@
 #ifndef ITVPLATFORM_H
 #define ITVPLATFORM_H
 
-#include <inttypes.h>
-#include <string>
-#include <vector>
+namespace WPEFramework {
+    namespace Exchange {
 
-enum TvmRc {
-    TvmSuccess = 0,
-    TvmError = -1
-};
+        struct ISectionParser {
+            virtual uint32_t Parse (const uint8_t data[], const uint16 datalength) = 0;
+            virtual uint32_t TableUpdate () = 0;
+        };
 
-class ChannelDetails {
-public:
-    ChannelDetails()
-        : type(Normal)
-    {
+        struct IMetaData {
+
+            enum source {
+                DVBT = 0,
+                DVBT2,
+                DVBC,
+                DVBC2,
+                DVBS,
+                DVBS2,
+                DVBH,
+                DVBSH,
+                ATSC,
+                ATSCMH,
+                ISDBT,
+                ISDBTB,
+                ISDBS,
+                ISDBC,
+                DTMB,
+                CMMB,
+                TDMB,
+                SDMB,
+                DVBS2X
+            };
+
+            enum type {
+                AUDIO = 0,
+                VIDEO,
+                SUBTITLE,
+                TELETEXT,
+                COMPOSITE
+            };
+
+            enum codec {
+                UNKNOWN = 0,
+                H264,
+                H265,
+                AAC
+            };
+
+            struct IIterator {
+                virtual bool IsValid() const = 0;
+                virtual void Reset() = 0;
+                virtual bool Next() = 0;
+                virtual IMetaData* MetaData() = 0;
+            }
+
+            virtual type Type() = 0;
+            virtual source Source() = 0;
+
+            struct IDVB : public IMetaData {
+
+                enum spectrum {
+                    AUTO,
+                    NORMAL,
+                    INVERTED
+                };
+
+                enum modulation {
+                    QAM_MODE_16,
+                    QAM_MODE_32,
+                    QAM_MODE_64,
+                    QAM_MODE_128,
+                    QAM_MODE_256,
+                    QAM_MODE_512,
+                    QAM_MODE_1024,
+                    QAM_MODE_2048,
+                    QAM_MODE_4096,
+                    QAM_MODE_AUTO,
+                    SAT_MODE_DVBS,
+                    SAT_MODE_QPSKTURBO,
+                    SAT_MODE_8PSKTURBO,
+                    SAT_MODE_TURBO,
+                    SAT_MODE_QPSKLDPC, //Same as DVBS2-QPSK
+                    SAT_MODE_8PSKLDPC, //Same as DVB-S2 8PSK
+                    SAT_MODE_DVBS216APSK, //DVB-S2 16APSK
+                    SAT_MODE_DVBS232APSK, //DVB-S2 32APSK
+                    SAT_MODE_LDPC, //Same as DVB-S2
+                    SAT_MODE_DVBS2xQPSK,
+                    SAT_MODE_DVBS2x8PSK,
+                    SAT_MODE_DVBS2x8APSK,
+                    SAT_MODE_DVBS2x16APSK,
+                    SAT_MODE_DVBS2x32APSK,
+                    SAT_MODE_BLINDACQUISITION
+                };
+
+                virtual uint32_t Frequency() const = 0;
+                virtual uint32_t SymbolRate() const = 0;
+                virtual spectrum Spectrum() const = 0;
+                virtual modulation Modulation() const = 0;
+                virtual uint16_t Pid() const = 0;
+                virtual codec Codec() const = 0;
+            };
+
+            typedef IDVB IATSC;
+        };
+
+        struct IStream {
+
+            struct IIterator {
+                virtual bool IsValid() const = 0;
+                virtual void Reset() = 0;
+                virtual bool Next() = 0;
+                virtual IStream* Stream() = 0;
+            }
+
+            virtual IStream::IIterator Streams ();
+            virtual const IMetaData* MetaData() const = 0;
+            virtual ITVPlatform::errorcode Speed(const uint32_t speed) = 0;
+        };
+
+        struct IDecoder {
+
+            virtual IStream* Stream ();
+            virtual ITVPlatform::errorcode Speed (const sint32_t speed); // 100 is normal playback forward. 0 = pause.
+            virtual sint32_t Speed () const; // 100 is normal playback forward. 0 = pause.
+        };
+
+        struct IFrontend {
+
+            virtual ITVPlatform::errorcode Tune (const IMetaData* parameters);
+            virtual ITVPlatform::errorcode Prime(const IMetaData::IIterator* parameters);
+            virtual ITVPlatform::errorcode AddFilter (struct ISectionParser*, uint16_t pid, uint16_t tableId);
+            virtual ITVPlatform::errorcode RemoveFilter (struct ISectionParser*);
+
+            virtual IDecoder* Acquire ();
+            virtual ITVPlatform::errorcode Release (IDecoder* decoder);
+        };
+
+        struct ITVPlatform {
+
+            enum errorcode {
+                ERROR_NONE = 0,
+                ERROR_NOT_SUPPORTED
+            };
+
+            static ITVPlatform* Instance();
+            virtual IFrontend* Aquire () = 0;
+            virtual ITVPlatform::errorcode Release (IFrontend*) = 0;
+        };
     }
-    enum Type {
-        Normal,
-        Radio,
-        Data
-    };
-
-    const char* typeStr[3] = { "Normal", "Radio", "Data" };
-    uint32_t frequency;
-    uint16_t programNumber;
-    Type type;
-    // Audio Languages.
-    // Subtitles.
-};
-
-class TSInfo {
-public:
-    TSInfo() = default;
-    uint32_t frequency;
-    uint16_t programNumber;
-    uint16_t videoPid;
-    uint16_t audioPid; // FIXME Multiple audio pids.
-    uint16_t videoPcrPid;
-    uint16_t audioPcrPid;
-    uint16_t pmtPid;
-    uint8_t videoCodec;
-    uint8_t audioCodec;
-};
-
-typedef std::vector<TSInfo> TSInfoList;
-typedef std::vector<ChannelDetails> ChannelMap;
-
-enum ScanningState {
-    Cleared,
-    Scanned,
-    Completed,
-    Stopped
-};
-
-namespace TVPlatform {
-
-class ITVPlatform {
-public:
-    class ISectionHandler {
-    public:
-        virtual void SectionDataCB(const std::string& msg) = 0;
-    };
-
-    class ITunerHandler {
-    public:
-        virtual void ScanningStateChanged(const ScanningState) = 0;
-        virtual void CurrentChannelChanged(const std::string&) = 0;
-        virtual void StreamingFrequencyChanged(uint32_t) = 0;
-    };
-
-    virtual TvmRc Init() = 0;
-    virtual TvmRc Deinit() = 0;
-    virtual TvmRc Tune(uint32_t) = 0;
-    virtual TvmRc Scan(std::vector<uint32_t>, ITunerHandler&) = 0;
-    virtual TvmRc Tune(TSInfo&, ITunerHandler&) = 0;
-    virtual TvmRc StopScanning() = 0;
-    virtual TvmRc GetChannelMap(ChannelMap&) = 0;
-    virtual TvmRc GetTSInfo(TSInfoList&) = 0;
-    virtual TvmRc Disconnect() = 0;
-    virtual std::vector<uint32_t>& GetFrequencyList() = 0;
-
-    virtual TvmRc SetHomeTS(uint32_t, uint32_t) = 0;
-    virtual TvmRc StartFilter(uint16_t, uint8_t, ISectionHandler*) = 0;
-    virtual TvmRc StopFilter(uint16_t, uint8_t) = 0;
-    virtual TvmRc StopFilters() = 0;
-    virtual void SetDbs(bool) = 0;
-    virtual void SetTuneParameters(const std::string&) = 0;
-    virtual bool IsScanning() = 0;
-};
-
-struct ISystemTVPlatform {
-    virtual ITVPlatform* GetInstance() = 0;
-};
-
-template <typename IMPLEMENTATION>
-class SystemTVPlatformType : public ISystemTVPlatform {
-private:
-    SystemTVPlatformType(const SystemTVPlatformType<IMPLEMENTATION>&) = delete;
-    SystemTVPlatformType<IMPLEMENTATION>& operator=(const SystemTVPlatformType<IMPLEMENTATION>&) = delete;
-
-public:
-    SystemTVPlatformType () {
-    }
-    virtual ~SystemTVPlatformType () {
-    }
-
-public:
-    virtual ITVPlatform* GetInstance() {
-        return (&_instance);
-    }
-
-private:
-    IMPLEMENTATION _instance;
-};
-
-} //namespace TVPlatform
-
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-TVPlatform::ISystemTVPlatform*  GetSystemTVPlatform();
-
-#ifdef __cplusplus
 }
-#endif
 
 #endif // ITVPLATFORM_H
